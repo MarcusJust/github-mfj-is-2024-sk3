57c76e6f15a343cfe7f5314e078516332c306645
Marcus Just <filip.marcus.just@gmail.com>
2024-10-24 16:27:52 +0200

    #1 idk

--- IS-Projekty/program001-vypis-rady/Program.cs
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -3,6 +3,8 @@ using System.Security.Cryptography.X509Certificates;
 
 class Program {
     static void Main() {
+
+        string again = "a";
         Console.Clear();
         Console.WriteLine("**************************");
         Console.WriteLine("****Výpis číselné řady****");
@@ -17,27 +19,40 @@ class Program {
         // int step = int.Parse(Console.ReadLine());
 
         //Vstup od uživatele lépe řešený
-        Console.Write("Zadejte první číslo: ");
-        int first;
-        while(!int.TryParse(Console.ReadLine(), out first)){
-            Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
-        };
-        Console.Write("Zadejte poslední číslo: ");
-        int last;
-        while(!int.TryParse(Console.ReadLine(), out last)){
-            Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
-        };
-        Console.Write("Zadejte po kolika chcete skákat: ");
-        int step;
-        while(!int.TryParse(Console.ReadLine(), out step)){
-            Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
-        };
-        
-        while(first < last){
-            first = first + step;
-            Console.WriteLine(first);
-        }
+        while(again == "a"){
+            Console.Write("Zadejte první číslo: ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)){
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+            };
+            Console.Write("Zadejte poslední číslo: ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)){
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+            };
+            Console.Write("Zadejte po kolika chcete skákat: ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)){
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+            };
 
-        Console.ReadKey();
+            Console.WriteLine(first);
+            while(first + step < last){
+                first = first + step;
+                Console.WriteLine(first);
+            }
+            string charsoucet;
+            Console.WriteLine("Zadejte číslo u kterého chcete sečíst cifry: ");
+            charsoucet = Console.ReadLine();
+            Array chars = charsoucet.ToArray();
+            List<int> CharsInt = new List<int>();
+            foreach(char ch in chars){
+                CharsInt.Add(Int32.Parse(ch.ToString()));
+                Console.WriteLine(ch.ToString());
+            }
+            
+            Console.WriteLine("Pokud chcete pokračovat stiskněte klávesu 'a'");
+            again = Console.ReadLine();
+        };
     }
 };
